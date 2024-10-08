3d13b1f76ceb905cd90113c22854545bb048bc8d
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-10-08 10:13:35 +0100

    added some initial utility classes for the colours

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Component } from "./components/carousel";
 
 function App() {
   return (
-    <main className=" min-h-screen max-w-[1024px] mx-auto gap-2 dark:bg-gray-800">
+    <main className="mx-auto min-h-screen  max-w-screen-xl dark:bg-gray-800">
       <h1 className="text-2xl dark:text-white">Flowbite React + Vite</h1>
       <DarkThemeToggle />
       <Component />
