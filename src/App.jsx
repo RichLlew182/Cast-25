c2a0dae5a971c4e4dd6142d3b3e2271e7a9ddf14
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-10-08 15:32:19 +0100

    added pages folder and created 404 page. Moved home into pages and setup reacr router to handle any pages other than home

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { MainMenu } from "./components/navbar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
-import { FooterBottom } from "./components/footer";
-import { useRef } from "react";
+import NotFound from "./pages/404";
+import Home from "./pages/home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
-  const footerRef = useRef(null)
-
-  const scrollToFooter = () => {
-    footerRef.current?.scrollIntoView({ behavior: 'smooth' });
-  };
-
   return (
-    <>
-
-      <header className="absolute w-full">
-        <MainMenu scrollToFooter={scrollToFooter}/>
-      </header>
-
-    <main className="container mx-auto flex  min-h-screen flex-col justify-center items-start gap-6 px-6">
-      <h1 className="text-4xl sm:text-6xl xl:text-8xl dark:text-white">Dynamic and versatile project delivery consultancy</h1>
-      <h2 className="text-3xl  xl:text-4xl dark:text-white">Website coming soon</h2>
-        <a href="#"onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="text-xl sm:text-2xl flex gap-2 font-syne font-bold text-licorice hover:text-white">
-      Get in touch <FontAwesomeIcon icon={faCircleArrowUp} /></a>
-      </main>
-      <FooterBottom footerRef={footerRef}/>
-    </>
-    
+    <Router>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+    </Routes>
+</Router>
   );
 }
 
