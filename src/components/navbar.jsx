0be93a07faafe8077157c1b774602983ded203e2
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-10-14 09:53:18 +0100

    added additional links to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,7 +11,11 @@ export function MainMenu( {scrollToFooter}) {
       </Navbar.Brand>
       <Navbar.Toggle id="navbarToggle" className="p-0 text-licorice focus:border-none  focus:outline-none focus:shadow-none hover:bg-transparent"/>
       <Navbar.Collapse className="font-syne font-medium">
-        <Navbar.Link href="#" onClick={(e) => {e.preventDefault();scrollToFooter()}}className="px-0 flex gap-2 text-2xl text-licorice md:hover:text-white hover:bg-transparent border-none">Get in touch <FontAwesomeIcon icon={faCircleArrowUp} /></Navbar.Link>
+        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">What we do</Navbar.Link>
+        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">How we do it</Navbar.Link>
+        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">Who we've worked with</Navbar.Link>
+        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">Testimonials</Navbar.Link>
+       <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">Start your project<FontAwesomeIcon icon={faCircleArrowUp} /></Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
