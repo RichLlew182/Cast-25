e4e572d82c0c2976a4e8210c3d5c743181244ba8
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-10-14 10:53:35 +0100

    added a custom theme to the navbar to set it to hide and show on different breakpoints

--- src/components/above-fold.jsx
+++ src/components/above-fold.jsx
@@ -15,8 +15,8 @@ export default function AboveFold({scrollToFooter}) {
     return (
       <>
   
-      <main ref={container} className="container mx-auto flex  flex-col justify-center items-start gap-6 py-72 px-6">
-        <h1 className="fade text-4xl lg:text-6xl xl:text-7xl dark:text-white">Dynamic and versatile project delivery consultancy</h1>
+      <main ref={container} className="container mx-auto flex  flex-col justify-center items-start gap-6 py-48 md:py-72 px-6">
+        <h1 className="fade text-5xl lg:text-7xl dark:text-white">Dynamic and versatile project delivery consultancy</h1>
         
           <a href="#"onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="fade text-xl sm:text-2xl flex gap-2 font-syne font-bold text-licorice hover:text-white">
         Start your project<FontAwesomeIcon icon={faCircleArrowUp} /></a>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,47 @@
 "use client";
-import { Navbar } from "flowbite-react";
+import { Flowbite, Navbar } from "flowbite-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
+
+
+const customTheme = {
+  navbar: {
+    collapse: {
+      base: "w-full xl:block xl:w-auto test",
+      list: "mt-4 flex flex-col xl:mt-0 xl:flex-row xl:space-x-6",
+      hidden: {
+        "on": "hidden",
+        "off": ""
+      }
+    },
+
+    toggle: {
+      base: 'text-white hover:bg-blue-700 focus:ring-blue-300', // Custom styles for toggle button
+    },
+    
+  },
+};
 
 export function MainMenu( {scrollToFooter}) {
   return (
 
+
+    <Flowbite theme={{ theme: customTheme }}>
     <header className="absolute w-full">
     <Navbar  fluid rounded className="container py-12 px-6 sm:px-6 mx-auto bg-transparent">
       <Navbar.Brand href="/">
         <span className="self-center whitespace-nowrap text-4xl font-syne font-semibold lining-nums">CAST25</span>
       </Navbar.Brand>
-      <Navbar.Toggle id="navbarToggle" className="p-0 text-licorice focus:border-none  focus:outline-none focus:shadow-none hover:bg-transparent"/>
-      <Navbar.Collapse className="font-syne font-medium">
-        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">What we do</Navbar.Link>
-        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">How we do it</Navbar.Link>
-        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">Who we've worked with</Navbar.Link>
-        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">Testimonials</Navbar.Link>
-       <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">Start your project<FontAwesomeIcon icon={faCircleArrowUp} /></Navbar.Link>
+      <Navbar.Toggle id="navbarToggle" className="xl:hidden p-0 text-licorice focus:border-none  focus:outline-none focus:shadow-none hover:bg-transparent"/>
+      <Navbar.Collapse className="z-10 bg-tomato font-syne font-medium">
+        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 md:py-2 xl:py-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">What we do</Navbar.Link>
+        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 md:py-2 xl:py-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">How we do it</Navbar.Link>
+        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 md:py-2 xl:py-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">Who we've worked with</Navbar.Link>
+        <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 md:py-2 xl:py-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">Testimonials</Navbar.Link>
+       <Navbar.Link href="#" onClick={(e) => { e.preventDefault(); scrollToFooter() }} className="px-0 md:py-2 xl:py-0 flex gap-2 text-xl text-licorice md:hover:text-white hover:bg-transparent border-none">Start your project<FontAwesomeIcon icon={faCircleArrowUp} /></Navbar.Link>
       </Navbar.Collapse>
       </Navbar>
       </header>
+      </Flowbite>
   );
 }
