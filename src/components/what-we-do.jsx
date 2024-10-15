6adfbad18c83d7e27a129cf38b964e9641499988
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-10-15 09:51:01 +0100

    added active states to the what we do section to show the different lists

--- src/components/what-we-do.jsx
+++ src/components/what-we-do.jsx
@@ -9,7 +9,50 @@ gsap.registerPlugin(ScrollTrigger)
 
 export default function WhatWeDo({scrollTo, refs}) {
 
-    const container = useRef();
+  const container = useRef();
+
+  const button1Ref = useRef(null);
+  const button2Ref = useRef(null);
+  const hr1Ref = useRef(null);
+  const hr2Ref = useRef(null)
+  const listRef1 = useRef(null);
+  const listRef2 = useRef(null);
+  
+  const handleActive = (buttonId) => {
+    if (buttonId === 1) {
+
+      console.log(button1Ref)
+
+      if (button1Ref.current.classList.contains('active')) {
+        return
+      } else {
+        button1Ref.current.classList.add('active');
+        button2Ref.current.classList.remove('active');
+        hr1Ref.current.classList.add('active')
+        hr2Ref.current.classList.remove('active');
+        listRef1.current.classList.add('active');
+        listRef2.current.classList.remove('active');
+      }
+    
+    } else if (buttonId === 2) {
+
+      if (button2Ref.current.classList.contains('active')) { 
+        return
+      } 
+
+      else {
+        button2Ref.current.classList.add('active');
+        button1Ref.current.classList.remove('active');
+        hr1Ref.current.classList.remove('active');
+        hr2Ref.current.classList.add('active');
+        listRef2.current.classList.add('active');
+        listRef1.current.classList.remove('active');
+      }
+
+      
+    }
+
+  };
 
     useGSAP(
       () => {
@@ -41,15 +84,15 @@ export default function WhatWeDo({scrollTo, refs}) {
               <h2 className="box text-4xl sm:text-5xl lg:text-6xl font-semibold w-full">What we do</h2>
             <div className=" flex flex-col lg:flex-row gap-6 md:gap-12">
               <div className="box flex flex-col px-0  gap-6 w-1/2">
-                <h3 className="box text-white text-3xl sm:text-4xl lg:text-5xl font-semibold cursor-pointer">Digital project management</h3>
-                <hr className="box" />
-                <h3 className="box text-gray-400 text-3xl sm:text-4xl lg:text-5xl font-semibold cursor-pointer">Creative Production</h3>
-                <hr className="box"/>
+                <h3 ref={button1Ref} onClick={() => handleActive(1)} className="active box text-gray-400 text-3xl sm:text-4xl lg:text-5xl font-semibold cursor-pointer">Digital project management</h3>
+                <hr ref={hr1Ref} className="active box" />
+                <h3 ref={button2Ref} onClick={() => handleActive(2)} className="box text-gray-400 text-3xl sm:text-4xl lg:text-5xl font-semibold cursor-pointer">Creative Production</h3>
+                <hr ref={hr2Ref} className="box"/>
                 <a href="#"onClick={(e) => { e.preventDefault(); e.stopPropagation(); scrollTo(refs.footer) }} className="box fade text-xl sm:text-2xl flex gap-2 font-syne font-bold text-tomato hover:text-orange">
                 Start your project<FontAwesomeIcon icon={faCircleArrowUp} /></a>
               </div>
               <div className="flex flex-col p-0 w-1/2">
-              <ul id="projectManagementList" className="flex  flex-col gap-6">
+              <ul ref={listRef1} id="projectManagementList" className="active flex flex-col gap-6">
                   <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Website builds / redesigns</li>
                   <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Content and tech migrations</li>
                   <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Rebranding projects</li>
@@ -58,13 +101,12 @@ export default function WhatWeDo({scrollTo, refs}) {
                   <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Email marketing campaigns</li>
                 </ul>
                 
-                <ul id="creativeProductionList" className="flex-col gap-6 hidden active:flex">
-                  <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Website builds / redesigns</li>
-                  <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Content and tech migrations</li>
-                  <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Rebranding projects</li>
-                  <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Localisation</li>
-                  <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Digital marketing activity</li>
-                  <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Email marketing campaigns</li>
+                <ul ref={listRef2} id="creativeProductionList" className="flex flex-col gap-6">
+                  <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Print (press, POS and OOH)</li>
+                  <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Social media (content production and campaign activity - incl. influencers)</li>
+                  <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">PR and events</li>
+                  <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Audio production</li>
+                  <li className="box text-2xl sm:text-3xl lg:text-4xl font-syne font-semibold">Video production</li>
               </ul>
                  
               </div>
