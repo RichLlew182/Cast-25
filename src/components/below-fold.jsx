48db1c6756ed447e1ba633c938ae2e992f77586a
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-10-28 12:02:39 +0000

    reduced size of testimonial images below fold

--- src/components/below-fold.jsx
+++ src/components/below-fold.jsx
@@ -39,11 +39,11 @@ export default function BelowFold({ refs }) {
           <div className=" flex flex-col md:flex-row gap-6 md:gap-12">
 
             <div className="box flex flex-col bg-white p-12 lg:p-14 gap-6">
-              <img src="/images/client-logos/RCSE.png" height="106" width="250" alt="Royal College of Surgeons of England Logo" />
+              <img src="/images/client-logos/RCSE.png" height="96" width="225" alt="Royal College of Surgeons of England Logo" />
               <blockquote className="text-base">“Andy was brilliant. He closed a complex project calmly and to clear timescales and we'd recommend him to anyone.”</blockquote>
               <p className="text-lg font-bold font-syne">James Pearson-Jadwat</p></div>
             <div className="box flex flex-col bg-white  p-12 lg:p-14 gap-6">
-              <img src="/images/client-logos/discovery-channel.png" height="106" width="250" alt="Discovery Channel Logo" />
+              <img src="/images/client-logos/discovery-channel.png" height="96" width="225" alt="Discovery Channel Logo" />
               <blockquote className="text-base">“Chris did a stellar job of building rapport with teams and gets the tough stuff done on time, on budget and with a keen attention to detail.”</blockquote>
               <p className="text-lg font-bold font-syne">Michelle Goff</p></div>
           </div>
