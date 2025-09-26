28a38c9ade3a8eb3eb11ceac46d710adffe5b9ea
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2025-09-26 09:25:33 +0100

    updated H1 and window title

--- src/components/above-fold.jsx
+++ src/components/above-fold.jsx
@@ -19,7 +19,8 @@ export default function AboveFold({ scrollTo, refs }) {
       <section id="above-fold" ref={container} className="bg-tomato">
         <MainMenu scrollTo={scrollTo} refs={refs} />
         <div className="container min-h-screen h-full mx-auto flex  flex-col justify-center items-start gap-6 py-36 xl:py-52 px-6">
-          <h1 className="fade text-4xl sm:text-5xl lg:text-6xl ">Dynamic and versatile project delivery consultancy</h1>
+          <h1 className="fade text-4xl sm:text-5xl lg:text-6xl ">Empowering teams to deliver with confidence
+          </h1>
 
           <a href="#" onClick={(e) => { e.preventDefault(); e.stopPropagation(); scrollTo(refs.workWithUs) }} className="fade text-xl sm:text-lg flex gap-2 font-syne font-bold text-licorice hover:text-white">
             Get started<FontAwesomeIcon icon={faCircleArrowUp} /></a>
